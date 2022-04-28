6b6825e93b650254f72773e249f3eeffe3e01e8b
shimizu-saffle <shimizu.saffle@gmail.com>
2022-04-28 13:38:32 +0900

    WhatIsOnomatopoeiaPage内のレイアウトを画面サイズに対する相対値で組み換え

--- lib/pages/what_is_onomatopoeia_page.dart
+++ lib/pages/what_is_onomatopoeia_page.dart
@@ -21,119 +21,153 @@ class WhatIsOnomatopoeiaPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                FirestoreQueryBuilder<HeaderImage>(
-                  pageSize: 7,
-                  query: headerImageQuery,
-                  builder: (context, snapshot, _) {
-                    if (snapshot.isFetching) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                    return CarouselSlider.builder(
-                      itemCount: snapshot.docs.length,
-                      itemBuilder: (context, index, _) {
-                        final _scrollIndicatorDots = List.generate(
-                            snapshot.docs.length, (index) => index);
-                        final headerImage = snapshot.docs[index].data();
-                        return Stack(
-                          children: [
-                            Expanded(
-                              child: Image.network(
-                                headerImage.imageUrl,
-                                fit: BoxFit.cover,
-                                width: MediaQuery.of(context).size.width,
-                              ),
-                            ),
-                            if (snapshot.docs.length > 1)
-                              Positioned.fill(
-                                child: Align(
-                                  alignment: const Alignment(0, 0.79),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: _scrollIndicatorDots.map(
-                                      (dotIndex) {
-                                        return dotIndex == index
-                                            ? const _SelectedDot()
-                                            : const _UnSelectedDot();
-                                      },
-                                    ).toList(),
-                                  ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  FirestoreQueryBuilder<HeaderImage>(
+                    pageSize: 7,
+                    query: headerImageQuery,
+                    builder: (context, snapshot, _) {
+                      if (snapshot.isFetching) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                      return CarouselSlider.builder(
+                        itemCount: snapshot.docs.length,
+                        itemBuilder: (context, index, _) {
+                          final _scrollIndicatorDots = List.generate(
+                              snapshot.docs.length, (index) => index);
+                          final headerImage = snapshot.docs[index].data();
+                          return Stack(
+                            children: [
+                              Expanded(
+                                child: Image.network(
+                                  headerImage.imageUrl,
+                                  fit: BoxFit.cover,
+                                  width: MediaQuery.of(context).size.width,
                                 ),
                               ),
-                          ],
-                        );
-                      },
-                      options: CarouselOptions(
-                        height: 240.9,
-                        viewportFraction: 1.0,
-                        scrollPhysics: snapshot.docs.length <= 1
-                            ? const NeverScrollableScrollPhysics()
-                            : const PageScrollPhysics(),
-                      ),
-                    );
-                  },
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 12,
-                    top: 6,
+                              if (snapshot.docs.length > 1)
+                                Column(
+                                  children: [
+                                    SizedBox(height: screenHeight / 3.5),
+                                    _ScrollIndicator(
+                                      index: index,
+                                      scrollIndicatorDots: _scrollIndicatorDots,
+                                    ),
+                                  ],
+                                ),
+                            ],
+                          );
+                        },
+                        options: CarouselOptions(
+                          height: screenHeight / 3,
+                          viewportFraction: 1.0,
+                          scrollPhysics: snapshot.docs.length <= 1
+                              ? const NeverScrollableScrollPhysics()
+                              : const PageScrollPhysics(),
+                        ),
+                      );
+                    },
                   ),
-                  child: SizedBox(
-                    height: 21,
-                    width: 28,
-                    child: InkWell(
-                      onTap: () => Navigator.of(context).pop(),
-                      child: Image.asset('assets/images/home_icon_white.png'),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 12,
+                      top: 6,
+                    ),
+                    child: SizedBox(
+                      height: 21,
+                      width: 28,
+                      child: InkWell(
+                        onTap: () => Navigator.of(context).pop(),
+                        child: Image.asset('assets/images/home_icon_white.png'),
+                      ),
                     ),
                   ),
+                ],
+              ),
+              SizedBox(height: screenHeight / 50),
+              const Text(
+                'オノマトペとは？',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
                 ),
-              ],
-            ),
-            const Spacer(flex: 1),
-            const Text(
-              'オノマトペとは？',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
               ),
-            ),
-            const Spacer(flex: 1),
-            const Text(
-              '自由な気持ちでオノマトペ',
-              style: TextStyle(
-                color: AnimalOnomatopoeiaColor.gray2,
-                fontSize: 16,
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-            const Spacer(flex: 1),
-            const Divider(
-              color: AnimalOnomatopoeiaColor.blue,
-              thickness: 2,
-              indent: 155,
-              endIndent: 155,
-            ),
-            const Spacer(flex: 1),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 26.8),
-              child: Text(
-                'オノマトペとは、自然界の音・声、物事の\n状態や動きなどを音(おん)で象徴的に表した\n語。音象徴語。擬音語・擬声語・擬態語など\nを意味します。\nここでは、きこえた音に対して自由な気持ちでオノマトペに置き換えてもらっています。よろしければ、こちらからご参加ください。',
+              SizedBox(height: screenHeight / 30),
+              const Text(
+                '自由な気持ちでオノマトペ',
                 style: TextStyle(
                   color: AnimalOnomatopoeiaColor.gray2,
                   fontSize: 16,
                   fontWeight: FontWeight.w300,
                 ),
               ),
-            ),
-            const Spacer(flex: 6),
-          ],
+              SizedBox(height: screenHeight / 30),
+              Divider(
+                color: AnimalOnomatopoeiaColor.blue,
+                thickness: 2,
+                indent: screenWidth / 3,
+                endIndent: screenWidth / 3,
+              ),
+              SizedBox(height: screenHeight / 30),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 26.8),
+                child: Text(
+                  'オノマトペとは、自然界の音・声、物事の状態や動きなどを音(おん)で象徴的に表した語。音象徴語。擬音語・擬声語・擬態語などを意味します。',
+                  style: TextStyle(
+                    color: AnimalOnomatopoeiaColor.gray2,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 26.8),
+                child: Text(
+                  'ここでは、きこえた音に対して自由な気持ちでオノマトペに置き換えてもらっています。よろしければ、こちらからご参加ください。',
+                  style: TextStyle(
+                    color: AnimalOnomatopoeiaColor.gray2,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _ScrollIndicator extends StatelessWidget {
+  const _ScrollIndicator({
+    Key? key,
+    required List<int> scrollIndicatorDots,
+    required this.index,
+  })  : _scrollIndicatorDots = scrollIndicatorDots,
+        super(key: key);
+
+  final List<int> _scrollIndicatorDots;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: _scrollIndicatorDots.map(
+        (dotIndex) {
+          return dotIndex == index
+              ? const _SelectedDot()
+              : const _UnSelectedDot();
+        },
+      ).toList(),
     );
   }
 }
